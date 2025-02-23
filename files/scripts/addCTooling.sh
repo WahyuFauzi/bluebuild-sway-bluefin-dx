2d4e85f96ba3f6a308f0040535a55f9a51a16a69
yuyuid <yuyuid@192.168.1.17>
2025-02-23 11:30:46 -0500

    dev: add homebrew, ctooling and neovim

--- files/scripts/addCTooling.sh
+++ files/scripts/addCTooling.sh
@@ -3,4 +3,4 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 set -o pipefail  # Ensure pipeline errors are caught
 
-brew install neovim
+rpm-ostree install cmake cairo-devel
--- files/scripts/addHomebrew.sh
+++ files/scripts/addHomebrew.sh
@@ -3,4 +3,4 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 set -o pipefail  # Ensure pipeline errors are caught
 
-brew install neovim
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
