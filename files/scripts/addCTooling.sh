7acf8e278e39a414ae0b0a7be05fbf6e453f97c9
Wahyu <my.uncle.wahyu@gmail.com>
2025-02-23 21:20:29 -0500

    dev: add neovim for development

--- files/scripts/addCTooling.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e  # Exit immediately if a command exits with a non-zero status
-set -o pipefail  # Ensure pipeline errors are caught
-
-rpm-ostree install cmake cairo-devel
